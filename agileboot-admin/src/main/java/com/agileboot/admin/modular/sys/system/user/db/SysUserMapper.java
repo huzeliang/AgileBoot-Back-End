126c53aa7a62cfeeba381d42231d646a2ed1759c
huzeliang <1120546174@qq.com>
2026-02-07 17:27:17 +0800

    fix: sql字段与数据库字段不一致

--- agileboot-admin/src/main/java/com/agileboot/admin/modular/sys/system/user/db/SysUserMapper.java
+++ agileboot-admin/src/main/java/com/agileboot/admin/modular/sys/system/user/db/SysUserMapper.java
@@ -53,7 +53,7 @@ public interface SysUserMapper extends BaseMapper<SysUserEntity> {
      * @param userId 用户ID
      * @return 权限列表
      */
-    @Select("SELECT DISTINCT m.perms "
+    @Select("SELECT DISTINCT m.permission "
         + "FROM sys_menu m "
         + " LEFT JOIN sys_role_menu rm ON m.menu_id = rm.menu_id "
         + " LEFT JOIN sys_user u ON rm.role_id = u.role_id "
@@ -70,7 +70,7 @@ public interface SysUserMapper extends BaseMapper<SysUserEntity> {
      * @param queryWrapper 条件选择器
      * @return 分页处理后的用户列表
      */
-    @Select("SELECT DISTINCT u.user_id, u.dept_id, u.username, u.nick_name, u.email "
+    @Select("SELECT DISTINCT u.user_id, u.dept_id, u.username, u.nickname, u.email "
         + " , u.phone_number, u.status, u.create_time "
         + "FROM sys_user u "
         + " LEFT JOIN sys_dept d ON u.dept_id = d.dept_id "
